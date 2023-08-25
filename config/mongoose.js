d6e7d6cc92470623d4c01d49f04dbe2833dbcca1
Raj Dave <135813814+rajdave050@users.noreply.github.com>
2023-08-25 12:02:20 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose'); //requiring mongoose
-mongoose.connect ( 'mongodb://127.0.0.1:27017/hospital_api_development'); //connecting to mongodb
+mongoose.connect ( 'mongodb+srv://rajdave050:Mongopass1.@cluster0.umf9w5n.mongodb.net/?retryWrites=true&w=majority'); //connecting to mongodb
 const db = mongoose.connection; //acquiring the connection
 db.on('error', console.error.bind(console, 'Error in connecting to MongoDB!')); //if error occured
 db.once('open', function() {
